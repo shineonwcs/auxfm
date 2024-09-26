7298a960cd56bff6f3ebb047c38a29e84d67cdb3
shineonwcs <justin@shineon.biz>
2024-09-26 18:03:13 -0500

    Update auxfm.sh

--- auxfm.sh
+++ auxfm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-bash /home/pi/install/scripts/update.sh
-bash /home/pi/install/scripts/deps.sh
-bash /home/pi/install/scripts/ftpconf.sh
-bash /home/pi/install/scripts/sound.sh
-bash /home/pi/install/scripts/fmtransmitter.sh
+bash /home/pi/auxfm/scripts/update.sh
+bash /home/pi/auxfm/scripts/deps.sh
+bash /home/pi/auxfm/scripts/ftpconf.sh
+bash /home/pi/auxfm/scripts/sound.sh
+bash /home/pi/auxfm/scripts/fmtransmitter.sh
 
