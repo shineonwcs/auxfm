8929c96e2132884970a766b56cf5f8862e979df3
shineonwcs <justin@shineon.biz>
2024-09-26 18:02:24 -0500

    Delete install directory

--- install/auxfm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bash /home/pi/install/scripts/update.sh
-bash /home/pi/install/scripts/deps.sh
-bash /home/pi/install/scripts/ftpconf.sh
-bash /home/pi/install/scripts/sound.sh
-bash /home/pi/install/scripts/fmtransmitter.sh
-
