141579d9ea101b885e850eab656cabb1880c57fd
shineonwcs <justin@shineon.biz>
2024-09-26 20:29:38 -0500

    Update shuffleplay50.sh

--- shuffleplay50.sh
+++ shuffleplay50.sh
@@ -3,3 +3,4 @@ ls /home/pi/music/*.wav | shuf -n 50 > playlist.tmp
 while IFS= read -r line; do  
 sudo /home/pi/fm_transmitter/fm_transmitter -f 91.3 $line | play $line  
 done < playlist.tmp  
+sudo shutdown now
