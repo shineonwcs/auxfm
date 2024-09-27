d60768b3838e64655aee9d3fe1b04163c2b7b1c4
shineonwcs <justin@shineon.biz>
2024-09-27 18:12:31 -0500

    Update auxfm.sh

--- auxfm.sh
+++ auxfm.sh
@@ -7,4 +7,5 @@ bash /home/pi/auxfm/scripts/fm_transmitter.sh
 mv /home/pi/auxfm/shuffleplay.sh /home/pi/
 mkdir /home/pi/music
 mv /home/pi/auxfm/FILENAME.wav /home/pi/music/
+mv /home/pi/auxfm/convert.sh /home/pi/music/
 sudo reboot now
