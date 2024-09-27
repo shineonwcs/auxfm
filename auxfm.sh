8f8a1b0fc3ab19bfba5d020f24ce0138d95889bc
shineonwcs <justin@shineon.biz>
2024-09-27 07:46:16 -0500

    Update auxfm.sh

--- auxfm.sh
+++ auxfm.sh
@@ -4,4 +4,4 @@ bash /home/pi/auxfm/scripts/deps.sh
 bash /home/pi/auxfm/scripts/ftpconf.sh
 bash /home/pi/auxfm/scripts/sound.sh
 bash /home/pi/auxfm/scripts/fm_transmitter.sh
-mv shuffleplay50.sh /home/pi/
+mv /home/pi/auxfm/shuffleplay50.sh /home/pi/
