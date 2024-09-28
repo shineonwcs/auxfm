4f1ecd90f4496fd82f9c2f02bfe2e8f4a1bc730d
shineonwcs <justin@shineon.biz>
2024-09-28 08:11:01 -0500

    removing redundancies

--- auxfm.sh
+++ auxfm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-bash /home/pi/auxfm/scripts/ftpconf.sh
-bash /home/pi/auxfm/scripts/sound.sh
-bash /home/pi/auxfm/scripts/fm_transmitter.sh
 cp /home/pi/auxfm/scripts/shuffleplay.sh /usr/bin/shuffleplay
 cp /home/pi/auxfm/scripts/soxconvert.sh /usr/bin/soxconvert
+cp alsa.conf /home/pi/.asoundrc
+mv alsa.conf /etc/asound.conf
+cp /home/pi/auxfm/vsftpd.conf /etc/
 reboot now
--- scripts/fm_transmitter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/pi/
-git clone https://github.com/markondej/fm_transmitter
-cd fm_transmitter
-make
