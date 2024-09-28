edd853caa6adda61118ea4e6a15c02df09fd0922
shineonwcs <justin@shineon.biz>
2024-09-28 07:04:02 -0500

    Update auxfm.sh
    fix spacing error

--- auxfm.sh
+++ auxfm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bash /home/pi/auxfm/scripts/ftpconf.sh
 bash /home/pi/auxfm/scripts/sound.sh
-bash/home/pi/auxfm/scripts/fm_transmitter.sh
+bash /home/pi/auxfm/scripts/fm_transmitter.sh
 cp /home/pi/auxfm/scripts/shuffleplay.sh /usr/bin/shuffleplay
 cp /home/pi/auxfm/scripts/soxconvert.sh /usr/bin/soxconvert
 reboot now
