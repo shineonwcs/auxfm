3801d13292ca8c0a87716970327fffeaa317064e
shineonwcs <justin@shineon.biz>
2024-09-27 21:56:50 -0500

    Update auxfm.sh

--- auxfm.sh
+++ auxfm.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-bash /home/pi/auxfm/scripts/update.sh
-bash /home/pi/auxfm/scripts/deps.sh
-bash /home/pi/auxfm/scripts/ftpconf.sh
-bash /home/pi/auxfm/scripts/sound.sh
+sudo bash /home/pi/auxfm/scripts/update.sh
+sudo bash /home/pi/auxfm/scripts/deps.sh
+sudo bash /home/pi/auxfm/scripts/ftpconf.sh
+sudo bash /home/pi/auxfm/scripts/sound.sh
 bash /home/pi/auxfm/scripts/fm_transmitter.sh
-mv /home/pi/auxfm/shuffleplay.sh /home/pi/
 mkdir /home/pi/music
 mv /home/pi/auxfm/FILENAME.wav /home/pi/music/
-cp /home/pi/auxfm/scripts/shuffleplay.sh /usr/bin/shuffleplay
-cp /home/pi/auxfm/scripts/soxconvert.sh /usr/bin/soxconvert
-reboot now
+sudo cp /home/pi/auxfm/scripts/shuffleplay.sh /usr/bin/shuffleplay
+sudo cp /home/pi/auxfm/scripts/soxconvert.sh /usr/bin/soxconvert
+sudo reboot now
